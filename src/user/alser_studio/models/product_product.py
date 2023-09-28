6c531e587b7c64b3fa137b228515d1eb1abeab32
Your Name <you@example.com>
2023-09-28 17:21:18 +0000

    ..

--- src/user/alser_studio/models/product_product.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-
-class ProductProduct(models.Model):
-    _inherit = "product.product"
-
-    x_studio_revisado = fields.Boolean(string='Revisado')
-    x_descripcion_corta = fields.Text(string=u'Descripción corta')
--- src/user/alser_studio/models/purchase_order_line.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-
-class PurchaseOrderLine(models.Model):
-    _inherit = "purchase.order.line"
-
-    x_studio_referencia_interna = fields.Char(string='Referencia interna')
